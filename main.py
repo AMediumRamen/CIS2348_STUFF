9b10b9cf50c678b5c5dedb790e323bf69f756820
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 15:39:31 -0500

    1.20.py

---
+++ main.py
@@ -0,0 +1,7 @@
+user_num1 = int(input("Enter a Integer:"))
+print("You Entered:",user_num1)
+print(user_num1,"squared is",user_num1*user_num1)
+print("and",user_num1,"cubed is",user_num1*user_num1*user_num1,"!!")
+user_num2 = int(input("Enter another Integer:"))
+print(user_num1,"+",user_num2,"is",user_num1+user_num2)
+print(user_num1,"*", user_num2, "is", user_num1*user_num2)

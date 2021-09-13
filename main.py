31341314298dccc64f93fa539f9aa843f6d71515
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 17:36:32 -0500

    3.19.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Ahmed Rahman PSID:1820239
+#LAB 3.19
+
+wall_height = int(input("Enter wall height (feet):"))
+wall_width = int(input("Enter wall width (feet):"))
+print("Wall area:",wall_height*wall_width ,"square feet")

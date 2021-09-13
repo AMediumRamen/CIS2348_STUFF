c78a338339a18c544f25589ae4d8e0167a2895d7
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 17:10:50 -0500

    2.19.py

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ lemon_juicefloat = float(lemon_juice)
 water_float = float(water)
 agave_nectarfloat = float(agave_nectar)
 servings_madefloat = float(servings_made)
-
 print(f'Lemonade ingredients - yields {servings_madefloat:.2f} servings')
 print(f'{lemon_juicefloat:.2f} cup(s) lemon juice')
 print(f'{water_float:.2f} cup(s) water')
 print(f'{agave_nectarfloat:.2f} cup(s) agave nectar')
 
 
-servings_required = input("How many servings would you like to make?\n")
+servings_required = input("How many servings would you like to make?")
+print('\n')
 servings_requiredfloat = float(servings_required)
 random = servings_requiredfloat/servings_madefloat
+print('\n')
 print(f'Lemonade ingredients - yields {servings_requiredfloat:.2f} servings')
 print(f'{lemon_juicefloat*random:.2f} cup(s) lemon juice')
 print(f'{water_float*random:.2f} cup(s) water')
 print(f'{agave_nectarfloat*random:.2f} cup(s) agave nectar')
 
-
+print('\n')
 print(f'Lemonade ingredients - yields {servings_requiredfloat:.2f} servings')
-print(f'{lemon_juicefloat*random/16:.2f} cup(s) lemon juice')
-print(f'{water_float*random/16:.2f} cup(s) water')
-print(f'{agave_nectarfloat*random/16:.2f} cup(s) agave nectar')
+print(f'{lemon_juicefloat*random/16:.2f} gallon(s) lemon juice')
+print(f'{water_float*random/16:.2f} gallon(s) water')
+print(f'{agave_nectarfloat*random/16:.2f} gallon(s) agave nectar')

9338b5e9132145b04739c1c930f668980451b0a7
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 17:19:42 -0500

    2.19.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@
 lemon_juice= input("Enter amount of lemon juice (in cups):\n")
 water = input("Enter amount of water (in cups):\n")
 agave_nectar = input("Enter amount of agave nectar (in cups):\n")
-servings_made = input("How many servings does this make?\n")
+servings_made = input("How many servings does this make?")
 lemon_juicefloat = float(lemon_juice)
 water_float = float(water)
 agave_nectarfloat = float(agave_nectar)
 servings_madefloat = float(servings_made)
-
+print('\n')
 print(f'Lemonade ingredients - yields {servings_madefloat:.2f} servings')
 print(f'{lemon_juicefloat:.2f} cup(s) lemon juice')
 print(f'{water_float:.2f} cup(s) water')
 print(f'{agave_nectarfloat:.2f} cup(s) agave nectar')
 print('\n')
-
 
 servings_required = input("How many servings would you like to make?")
 servings_requiredfloat = float(servings_required)

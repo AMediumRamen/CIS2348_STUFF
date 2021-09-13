1c83380bd1d62a5fbc7f4e8852392478f22b8f3b
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 15:24:52 -0500

    1.12.py

---
+++ 1.12.py
@@ -0,0 +1,6 @@
+#Ahmed Rahman PSID:1820239
+userNum = int(input())
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+print(userNumSquared) # Output formatting issue here; fix it when instructed
+

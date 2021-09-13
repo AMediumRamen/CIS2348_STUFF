f1aef3c5c40abd67d707cb17cc4b42be2140d54b
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 18:12:01 -0500

    3.19

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #Ahmed Rahman PSID:1820239
 #LAB 3.19
 
-wall_height = int(input("Enter wall height (feet):"))
-wall_width = int(input("Enter wall width (feet):"))
+wall_height = int(input("Enter wall height (feet):\n"))
+wall_width = int(input("Enter wall width (feet):\n"))
 wall_area = wall_width*wall_height
 print("Wall area:",wall_area,"square feet")
 
@@ -13,5 +13,5 @@ cans_needed = round(gallons_needed)
 print("Cans needed:",cans_needed, "can(s)")
 
 colors_of_paint = {'red':35,'blue':25,'green':23}
-colorneeded = input("Choose a color to paint the wall:")
-print("Cost of purchasing red paint: $",colors_of_paint[colorneeded])
+colorneeded = input("Choose a color to paint the wall:\n")
+print("Cost of purchasing red paint: $"+colors_of_paint[colorneeded])

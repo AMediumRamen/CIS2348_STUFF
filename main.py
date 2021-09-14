0e83d5cf64691c3a78de7a491a6ec9a559c853a3
AMediumRamen <naimurrahmanloban@gmail.com>
2021-09-13 20:00:43 -0500

    5.22

---
+++ main.py
@@ -0,0 +1,20 @@
+#Ahmed Rahman PSID:1820239
+#LAB 5.22
+print("Davy's auto shop services")
+print("Oil change -- $35")
+print("Tire rotation -- $19")
+print("Car wash -- $7")
+print("Car wax -- $12")
+
+services = {'Oil change':35,'Tire rotation':19,'Car wash':7,'Car wax':12,'No service':0}
+first_service = input("Select first service:")
+second_service = input("Select second service:")
+
+print("Davy's auto shop invoice")
+print('')
+print("Service 1: ",first_service,", $",services[first_service],sep='')
+print("Service 1: ",second_service,", $",services[second_service],sep='')
+total = services[first_service]+services[second_service]
+print('')
+print('Total: $',total,sep='')
+

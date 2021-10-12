17ea0e1a554f4bafa286c57231b6723731a79f60
naimu <naimurrahmanloban@gmail.com>
2021-10-12 11:49:34 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+Old = input('')
+Old = Old.replace('i','!')
+Old = Old.replace('a','@')
+Old = Old.replace('m','M')
+Old = Old.replace('B','8')
+Old = Old.replace('o','.')
+
+New = Old + 'q*s'
+print(New)

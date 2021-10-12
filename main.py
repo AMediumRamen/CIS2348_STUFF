7ff444ba1602dc0e919caaa810e1ea752bab327b
naimu <naimurrahmanloban@gmail.com>
2021-10-12 12:31:25 -0500

    6.22

---
+++ main.py
@@ -0,0 +1,17 @@
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+fifth = int(input())
+sixth = int(input())
+
+for x in range(-10, 11):
+    for y in range(-10,11):
+        if first * x + second * y == third and  fourth* x + fifth * y == sixth:
+            print(x, y)
+            yikes = 1
+if yikes != 1:
+    print('No Solution')
+
+
+

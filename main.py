62203fb376f25ee73c24f7d51aed9f2f370df352
naimu <naimurrahmanloban@gmail.com>
2021-10-13 13:25:15 -0500

    9.10

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import csv
-yikes = open('input1.csv')
-type(yikes)
-words = csv.reader(yikes)
-for word in words:
-    yikes_list = list(words)
-    for a in yikes_list:
-        b = word.count(a)
-        print(a,b)
+with open('input1.csv','r') as yikes:
+    words = csv.reader(yikes)
+    for word in words:
+        yikes_list = list(words)
+        for a in yikes_list:
+            b = word.count(a)
+            print(a,b)

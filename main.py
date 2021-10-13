494620f3f289ade8dab2b18b6892a19f90e30d62
naimu <naimurrahmanloban@gmail.com>
2021-10-13 14:11:40 -0500

    9.10

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ inputfile = input()
 with open(inputfile, 'r') as yikes:
     yikers = csv.reader(yikes)
     for anything in yikers:
-        word_list = set(anything)
-no_duplicates_in_list = list(dict.fromkeys(word_list))
-listlength = len(no_duplicates_in_list)
-
-for i in range(listlength):
-    print(no_duplicates_in_list[i], word_list.count(no_duplicates_in_list[i]))
-
+        word_list = list(set(anything))
+        for word in word_list:
+            word_count = anything.count(word)
+            print(word, word_count)

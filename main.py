5b2e9810e6086054370286fabfe7983a5e297d16
naimu <naimurrahmanloban@gmail.com>
2021-10-13 14:15:29 -0500

    9.10

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ inputfile = input()
 with open(inputfile, 'r') as yikes:
     yikers = csv.reader(yikes)
     for anything in yikers:
-        word_list = list(set(anything))
+        word_list = list(anything)
         for word in word_list:
             word_count = anything.count(word)
             print(word, word_count)

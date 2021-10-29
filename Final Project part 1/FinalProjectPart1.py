adcc44abe9b95ee6b2288646755163f3e85af707
naimu <naimurrahmanloban@gmail.com>
2021-10-29 13:57:39 -0500

    coding headache

--- Final Project part 1/FinalProjectPart1.py
+++ Final Project part 1/FinalProjectPart1.py
@@ -1,24 +1,86 @@
 #Ahmed Rahman PSID:1820239
 import csv
-ManList = csv.reader(open('ManufacturerList.csv'))
-ManDict = {}
-for row in ManList:
-    key = row[0]
-    ManDict[key] = row[1:2]
+import datetime
 
-PriceList = csv.reader(open('PriceList.csv'))
+yikes = open('FullInventory.csv','w')
+
+with open('ManufacturerList.csv','r') as Man_csv:
+    reader = csv.reader(Man_csv,delimiter=',')
+    ManList=[]
+    for row in reader:
+        ManList.append([int(row[0]),row[1],row[2],row[3]])
+
+
+IDList = []
+for sublist in ManList:         #List of IDs Only
+    IDList.append(sublist[0])
+ID = tuple(IDList)              #Had to change to a tuple to avoid errors
+
+
+ManDict={}                      #Dictionary of Manufacturers
+for row in ManList:
+    key = int(row[0])
+    ManDict[key] = row[1]
+
+
+
+typeDict = {}   #Dictionary of types
+for row in ManList:
+    key = int(row[0])
+    typeDict[key] = row[2]
+
+
+
+PriceList = csv.reader(open('PriceList.csv'))    #Dictionary of Prices
 PriceDict = {}
 for row in PriceList:
-    key = row[0]
-    PriceDict[key]=row[1]
+    key = int(row[0])
+    PriceDict[key]=int(row[1])
 
-ServiceDatesList = csv.reader(open('ServiceDatesList.csv'))
+
+ServiceDatesList = csv.reader(open('ServiceDatesList.csv'))   #Dictionary of Serice Dates
 ServiceDatesDict = {}
 for row in ServiceDatesList:
-    key = row[0]
-    ServiceDatesDict[key]=row[1]
+    key = int(row[0])
+    ServiceDatesDict[key]=datetime.datetime.strptime(row[1],'%m/%d/%Y')
 
-print(ManDict)
+DamagedDict={}
+for row in ManList:
+    key = int(row[0])
+    DamagedDict[key]=row[3]
+
+sortedArray = []
+for ids in ID:
+    x = list((ids,ManDict.get(ids),typeDict.get(ids),PriceDict.get(ids),ServiceDatesDict.get(ids),DamagedDict.get(ids)))
+    sortedArray.append(x)
+
+sortedArray.sort(key=lambda a:a[1])
+print(sortedArray)
+
+#for ids in ID:
+#    x1 = list([ids,ManDict.get(ids),typeDict.get(ids),PriceDict.get(ids),ServiceDatesDict.get(ids),DamagedDict.get(ids)])
+#    fullArray = [x1]
+#    fullArray.sort(key=lambda a:a[1])
+#    print(fullArray)
+#    yikes.write(str(fullArray))
+#    yikes.write('\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

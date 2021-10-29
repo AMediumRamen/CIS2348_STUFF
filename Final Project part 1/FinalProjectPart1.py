866e9759502668f189ac76f4ecd2044962d411b1
naimu <naimurrahmanloban@gmail.com>
2021-10-29 00:12:11 -0500

    coding headache

--- Final Project part 1/FinalProjectPart1.py
+++ Final Project part 1/FinalProjectPart1.py
@@ -1,2 +1,16 @@
 #Ahmed Rahman PSID:1820239
-ManList = open('ManufacturerList.csv','r')
+ManList = list(open('ManufacturerList.csv','r'))
+PriceList = list(open('PriceList.csv','r'))
+ServiceDatesList = list(open('ServiceDatesList.csv','r'))
+
+def CovertToDict(list):
+    iterate = iter(list)
+    converted = dict(zip(iterate,iterate))
+    return converted
+PriceDict = CovertToDict(PriceList)
+print(PriceDict)
+
+
+
+
+

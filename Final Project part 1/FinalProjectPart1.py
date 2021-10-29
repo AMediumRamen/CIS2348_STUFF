c948eedd92a86887012bf5e1bc6695a66d8aa2da
naimu <naimurrahmanloban@gmail.com>
2021-10-29 01:25:51 -0500

    coding headache

--- Final Project part 1/FinalProjectPart1.py
+++ Final Project part 1/FinalProjectPart1.py
@@ -1,14 +1,29 @@
 #Ahmed Rahman PSID:1820239
-ManList = list(open('ManufacturerList.csv','r'))
-PriceList = list(open('PriceList.csv','r'))
-ServiceDatesList = list(open('ServiceDatesList.csv','r'))
+import csv
+ManList = csv.reader(open('ManufacturerList.csv'))
+ManDict = {}
+for row in ManList:
+    key = row[0]
+    ManDict[key] = row[1:2]
 
-def CovertToDict(list):
-    iterate = iter(list)
-    converted = dict(zip(iterate,iterate))
-    return converted
-PriceDict = CovertToDict(PriceList)
-print(PriceDict)
+PriceList = csv.reader(open('PriceList.csv'))
+PriceDict = {}
+for row in PriceList:
+    key = row[0]
+    PriceDict[key]=row[1]
+
+ServiceDatesList = csv.reader(open('ServiceDatesList.csv'))
+ServiceDatesDict = {}
+for row in ServiceDatesList:
+    key = row[0]
+    ServiceDatesDict[key]=row[1]
+
+print(ManDict)
+
+
+
+
+
 
 
 

268393d93564e715b587abf67f4ed2dd25ef28b4
naimu <naimurrahmanloban@gmail.com>
2021-11-10 13:22:10 -0600

    coding headache

--- Homework 3 stuff/main.py
+++ Homework 3 stuff/main.py
@@ -1,13 +1,13 @@
 class FoodItem:
-    def __init__(self,name = "None",fat=0.0,carbs=0.0,protein=0.0,servings=0.0):
-        self.name = name
-        self.fat = fat
-        self.carbs= carbs
-        self.protein = protein
-        self.servings = servings
+    def __init__(self,Foodname = "None",Foodfat=0.0,Foodcarbs=0.0,Foodprotein=0.0,Foodservings=0.0):
+        self.name = Foodname
+        self.fat = Foodfat
+        self.carbs= Foodcarbs
+        self.protein = Foodprotein
+        self.servings = Foodservings
 
-    def get_calories(self,servings):
-        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * servings
+    def get_calories(self,Foodservings):
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * Foodservings
         return calories
     def print_info(self):
         print('Nutritional information per serving of {}:'.format(self.name))
@@ -15,7 +15,18 @@ class FoodItem:
         print('   Carbohydrates: {:.2f} g'.format(self.carbs))
         print('   Protein: {:.2f} g'.format(self.protein))
 
-if __name__ == "main":
+if __name__ == "__main__":
     first = FoodItem()
-    first.print_info()
+    Foodname = input()
+    Foodfat = float(input())
+    Foodcarbs = float(input())
+    Foodprotein = float(input())
+    Foodservings = float(input())
 
+    second = FoodItem(Foodname,Foodfat,Foodcarbs,Foodprotein)
+
+    first.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(Foodservings,first.get_calories(Foodservings)))
+
+    second.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(Foodservings, second.get_calories(Foodservings)))

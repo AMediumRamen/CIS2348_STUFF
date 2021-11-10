d2f60e55462161e98649bee32b0f8aa847477d4e
naimu <naimurrahmanloban@gmail.com>
2021-11-10 14:18:07 -0600

    Coding Headache

--- Homework 3 stuff/main.py
+++ Homework 3 stuff/main.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     yikes.teamWins = wins
     yikes.teamLosses = loss
 
-    if yikes.get_win_percentage()>0.5:
+    if yikes.get_win_percentage()>=0.5:
         print('Congratulations, Team {} has a winning average!'.format(name))
     else:
         print('Team {} has a losing average.'.format(name))

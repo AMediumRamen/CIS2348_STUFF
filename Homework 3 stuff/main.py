d5a2df40999047031f7b65bd337a3a591eb6c9de
naimu <naimurrahmanloban@gmail.com>
2021-11-10 12:45:37 -0600

    coding headache

--- Homework 3 stuff/main.py
+++ Homework 3 stuff/main.py
@@ -1,10 +1,11 @@
 class FoodItem:
-    def __init__(self,name,fat,carbs,protein,servings):
-        self.name = "None"
-        self.fat = 0.0
-        self.carbs = 0.0
-        self.protein = 0.0
-        self.servings = 0.0
+    def __init__(self,name = "None",fat=0.0,carbs=0.0,protein=0.0,servings=0.0):
+        self.name = name
+        self.fat = fat
+        self.carbs= carbs
+        self.protein = protein
+        self.servings = servings
+
     def get_calories(self,servings):
         calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * servings
         return calories
@@ -14,5 +15,7 @@ class FoodItem:
         print('   Carbohydrates: {:.2f} g'.format(self.carbs))
         print('   Protein: {:.2f} g'.format(self.protein))
 
-
+if __name__ == "main":
+    first = FoodItem()
+    first.print_info()
 

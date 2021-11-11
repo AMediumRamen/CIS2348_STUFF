e7e76267df5d50e55065004553934a84d5730414
naimu <naimurrahmanloban@gmail.com>
2021-11-11 00:40:29 -0600

    Coding Headache

---
+++ Homework 3 stuff/main.py
@@ -0,0 +1,14 @@
+#Ahmed Rahman PSID:1820239
+
+listMaker = input()
+stringList = listMaker.split()
+stringDictionary = {}
+
+for i in stringList:
+    if i in stringDictionary:
+        stringDictionary[i] += 1
+    else:
+        stringDictionary[i] = 1
+
+for keys in stringDictionary:
+    print(keys,stringDictionary.get(keys))

1e6f16e979a6533752c4923b8ba3917c221ca429
naimu <naimurrahmanloban@gmail.com>
2021-11-29 18:56:14 -0600

    Coding Headache

---
+++ Homework 4 stuff/main.py
@@ -0,0 +1,13 @@
+#Ahmed Rahman PSID: 1820239
+
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+    except ValueError as yikes:
+        print(0)
+
+    parts = input().split()
+    name = parts[0]

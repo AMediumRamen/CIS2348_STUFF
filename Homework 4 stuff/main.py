bbb25a1da7a8b0b935b827148ee5ad539d06fe74
naimu <naimurrahmanloban@gmail.com>
2021-12-01 01:05:21 -0600

    Coding Headache

--- Homework 4 stuff/main.py
+++ Homework 4 stuff/main.py
@@ -8,7 +8,7 @@ def selection_sort_descend_trace(numbers):
             if numbers[b] > numbers[num_big]:
                 num_big = b
         numbers[a],numbers[num_big]=numbers[num_big],numbers[a]
-        print(' '.join([str(x)for x in numbers]),end='\n')
+        print(' '.join([str(x)for x in numbers]),end=' \n')
 
     return numbers
 if __name__ == "__main__":

8764f1e47053a8b9c7019e75a1320c660086451f
naimu <naimurrahmanloban@gmail.com>
2021-12-01 00:41:54 -0600

    Coding Headache

--- Homework 4 stuff/main.py
+++ Homework 4 stuff/main.py
@@ -6,10 +6,8 @@ def selection_sort_descend_trace(numbers):
         for b in range(a+1 and len(numbers)):
             if numbers[b] > numbers[num_big]:
                 num_big = b
-        numbers[a],numbers[num_big] = numbers[num_big],numbers[a]
-        for x in range(len(numbers)):
-            print(numbers[x],end='')
-            print('\n')
+            numbers[a],numbers[num_big] = numbers[num_big],numbers[a]
+        print(' '.join([str(x)for x in numbers]))
 
     return numbers
 

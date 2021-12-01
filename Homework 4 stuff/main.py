f058073c18afd4ffd5e0a01cef46743795ad6ecd
naimu <naimurrahmanloban@gmail.com>
2021-12-01 01:00:29 -0600

    Coding Headache

--- Homework 4 stuff/main.py
+++ Homework 4 stuff/main.py
@@ -1,21 +1,21 @@
 #Ahmed Rahman PSID:1820239
 
 def selection_sort_descend_trace(numbers):
-    for a in range(len(numbers)-1):
+    totalN = len(numbers)
+    for a in range(totalN-1):
         num_big = a
-        for b in range(a+1 and len(numbers)):
+        for b in range(a+1, totalN):
             if numbers[b] > numbers[num_big]:
                 num_big = b
-            numbers[a],numbers[num_big] = numbers[num_big],numbers[a]
+                temp = numbers[a]
+                numbers[a] = numbers[num_big]
+                numbers[num_big]=temp
         print(' '.join([str(x)for x in numbers]))
 
     return numbers
-
-
-
-
-
 if __name__ == "__main__":
     num = input().split()
-    numbers = [int(x) for x in num]
+    numbers = []
+    for x in num:
+        numbers.append(int(x))
     selection_sort_descend_trace(numbers)

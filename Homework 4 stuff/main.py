ca7b029f5d309220a2f1ea0c037497626d5527d3
naimu <naimurrahmanloban@gmail.com>
2021-11-30 23:45:10 -0600

    Coding Headache

--- Homework 4 stuff/main.py
+++ Homework 4 stuff/main.py
@@ -4,14 +4,13 @@ list_to_sort = list[int(input())]
 def selection_sort_descend_trace(numbers):
     iter1 = 0
     iter2 = iter1+1
-    newList = []
     while iter1 != len(numbers):
         num_big = numbers[iter1]
         if numbers[iter2] > num_big:
             num_big = numbers[iter2]
-            newList[iter1]=num_big
             iter1 +=1
-
+        numbers[iter1],numbers[num_big] = numbers[num_big],numbers[iter1]
+    print(numbers)
 
 
 

716297b3b83b98b3e3af7c66ff0a25a96d846ce4
naimu <naimurrahmanloban@gmail.com>
2021-12-01 00:39:25 -0600

    Coding Headache

--- Homework 4 stuff/main.py
+++ Homework 4 stuff/main.py
@@ -8,7 +8,8 @@ def selection_sort_descend_trace(numbers):
                 num_big = b
         numbers[a],numbers[num_big] = numbers[num_big],numbers[a]
         for x in range(len(numbers)):
-            print(numbers[x])
+            print(numbers[x],end='')
+            print('\n')
 
     return numbers
 

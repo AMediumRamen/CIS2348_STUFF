666d375815ed2cfd5e380eecb8978e0842b9d281
naimu <naimurrahmanloban@gmail.com>
2021-12-01 22:25:03 -0600

    Coding Headache

--- Homework 4 stuff/14.13.py
+++ Homework 4 stuff/14.13.py
@@ -10,10 +10,12 @@ def partition(user_ids, i, k):
         while user_ids[k] > piv:#decrement through the right side while > varible piv
             k -= 1
         if i <= k: #determines if the swap is necessary based on if number at user_ids[i] is greater than user_ids[k]
+            break
+        else:#actual swap
             user_ids[i],user_ids[k] = user_ids[k],user_ids[i]
-            i += 1
-            k -= 1
-        return k
+            i += 1 #update left increment
+            k -= 1 #update right increment
+    return k
 
 
 
@@ -22,7 +24,7 @@ def quicksort(user_ids,i,k):
     num_calls += 1  # increments the global variable after each call of quicksort function
     if i >= k: #checking if sorted before calling partition function
         return
-    part = partition(user_ids,i,k)
+    part = partition(user_ids,i,k)#calling partition function to determine the last value in the left partition
     quicksort(user_ids,i,part)#sort from i=0 to part(the pivot)-1
     quicksort(user_ids,part+1,k)#sort from part(pivot)+1 to k(last index)
     return

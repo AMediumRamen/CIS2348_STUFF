22cdc44104c13fa9d0710d2df2bf6a2117626b84
naimu <naimurrahmanloban@gmail.com>
2021-12-01 22:38:06 -0600

    Coding Headache

--- Homework 4 stuff/12.9.py
+++ Homework 4 stuff/12.9.py
@@ -1,9 +1,10 @@
 #Ahmed Rahman PSID: 1820239
+#SKELETON CODE WAS PROVIDED IN LAB 12.8
 
 parts = input().split()
 name = parts[0]
 while name != '-1':
-    try:
+    try:#implementation of try block as per FIXME instructions
         age = int(parts[1]) + 1
         print('{} {}'.format(name, age))
     except ValueError as yikes:

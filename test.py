7944ab93ef631c227d006554d02435dd1c7c1d92
naimu <naimurrahmanloban@gmail.com>
2021-12-01 23:47:14 -0600

    Coding Headache

--- test.py
+++ test.py
@@ -1,11 +1,54 @@
-num = input().split()
-numbers = [int(x) for x in num]
-iter1 = 0
-iter2 = iter1 + 1
-while iter1 != (len(numbers)-1):
-    num_big = numbers[iter1]
-    if numbers[iter2] > num_big:
-        num_big = numbers[iter2]
-        iter1 += 1
-    numbers[iter1], numbers[num_big] = numbers[num_big], numbers[iter1]
-print(numbers)
+# There are different ways to do a Quick Sort partition, this implements the
+# Hoare partition scheme. Tony Hoare also created the Quick Sort algorithm.
+num_calls = 0
+def partition(nums, low, high):
+    # We select the middle element to be the pivot. Some implementations select
+    # the first element or the last element. Sometimes the median value becomes
+    # the pivot, or a random one. There are many more strategies that can be
+    # chosen or created.
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # If an element at i (on the left of the pivot) is larger than the
+        # element at j (on right right of the pivot), then swap them
+        nums[i], nums[j] = nums[j], nums[i]
+
+
+def quick_sort(nums):
+    # Create a helper function that will be called recursively
+    global num_calls
+    num_calls += 1
+    def _quick_sort(items, low, high):
+        if low < high:
+            # This is the index after the pivot, where our lists are split
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+if __name__ == "__main__":
+    user_ids = []
+    user_id = input()
+    while user_id != "-1":
+        user_ids.append(user_id)
+        user_id = input()
+
+    quick_sort(user_ids, 0, len(user_ids) - 1)
+
+    print(num_calls)
+
+    for user_id in user_ids:
+        print(user_id)
